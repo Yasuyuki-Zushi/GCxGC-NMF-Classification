50b8f3cb3cd09e0176a832afb52102115b8d96a4
Yasuyuki-Zushi <34370361+Yasuyuki-Zushi@users.noreply.github.com>
2017-12-20 18:20:14 +0900

    Delete cosine.dist.r

--- GCｘGC-NMF-Classification_v1.1.0/Code/cosine.dist.r
+++
@@ -1,8 +0,0 @@
-cosine.dist <- function(x) 
-{ 
-x <- as.matrix(x) 
-ss <- 1/sqrt(colSums(x^2)) 
-col.similarity <- t(x) %*% x *outer(ss, ss) 
-colnames(col.similarity) <- rownames(col.similarity) <- colnames(x) 
-return(col.similarity) 
-} 
